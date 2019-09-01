2a3b377e4aa82b2c434a1fa8b588143355634974
cibergarri <cibergarri@hotmail.com>
2019-09-02 00:05:07 +0200

    add notifications model

---
+++ src/models/notification.js
@@ -0,0 +1,11 @@
+import { model, Schema } from 'mongoose';
+
+const NotificationSchema = new Schema({
+  type: {type: String, enum: ['mail', 'push'], required: [true, 'type is required']},
+  title: { type: String },
+  body: { type: String },
+  origin: { type: Schema.Types.ObjectId, ref: 'User' },
+  destination: { type: Schema.Types.ObjectId, ref: 'User' },
+  status: {type: String, enum: ['pending', 'sent', 'errored'], default: 'pending'},
+});
+export const Notification = model('Notification', NotificationSchema);
