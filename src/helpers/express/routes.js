536c8ac9629f0d9482d6f26a8800a166e4a84828
cibergarri <cibergarri@hotmail.com>
2019-09-01 19:36:43 +0200

    fix routes

--- src/helpers/express/routes.js
+++ src/helpers/express/routes.js
@@ -1,20 +1,20 @@
-import { Router } from 'express';
+import express from 'express';
 import { usersRoute } from '../../endpoints/users';
 import serve from 'express-static';
 
 // import { docsRoute } from '../swagger/docs';
 
-const apiRoutes = Router();
+const apiRoutes = express.Router();
 apiRoutes.use('/users', usersRoute);
 
 const rootRoute = (req, res) => {
   res.send('Welcome!');
 };
 
-export const routes = Router();
+export const routes = express.Router();
 // routes.use(logRequestsMiddleware);
 // routes.use('/api-docs', docsRoute);
 routes.use('/api', apiRoutes);
 routes.get('/alive', rootRoute);
 routes.use('/push', serve(__dirname + '../../../../static/push'));
-routes.get('/', serve(__dirname + '../../../../app/dist/app'));
+routes.use('/', serve(__dirname + '../../../../app/dist/app'));
