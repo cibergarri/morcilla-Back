b657ee8e19640887a1e2d32a519b62ee4ec1d1b7
ruben <ruben@DESKTOP-JQGSS77>
2019-09-05 12:21:05 +0200

    auto clock-out on logout

--- app/src/app/services/clock-in.service.ts
+++ app/src/app/services/clock-in.service.ts
@@ -1,19 +1,24 @@
+import { switchMap } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { environment } from 'src/environments/environment';
 
 @Injectable()
-export class ClockInService{
+export class ClockInService {
 
-    constructor(private http:HttpClient){
+    constructor(private http: HttpClient, private auth: AuthService) {
+        this.auth.logoutEvents.pipe(switchMap(() => {
+            return this.clockOut();
+        })).subscribe(() => { });
 
     }
 
-    clockIn(){
+    clockIn() {
         return this.http.post(environment.apiUrl + "/api/clock/in", {});
     }
 
-    clockOut(){
+    clockOut() {
         return this.http.post(environment.apiUrl + "/api/clock/out", {});
     }
 }
