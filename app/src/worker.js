4a64d599a165a72f9d87709f5d2a7ff99b825d55
cibergarri <cibergarri@hotmail.com>
2019-09-05 02:16:09 +0200

    testing notifications

--- app/src/worker.js
+++ app/src/worker.js
@@ -3,8 +3,12 @@ console.log('Loaded service worker!');
 self.addEventListener('push', ev => {
   const data = ev.data.json();
   console.log('Got push', data);
+  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
   self.registration.showNotification(data.title, {
-    body: data.title,
-    icon: '/push/morcilla.jpg'
+    body: data.body,
+    icon: data.icon || '/push/morcilla.jpg',
+    image: data.image || '/push/morcilla.jpg',
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    requireInteraction: true,
   });
 });
